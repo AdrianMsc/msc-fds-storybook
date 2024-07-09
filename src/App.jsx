a02cbbe9a36975db98760b5f443305d521700cc0
diegomsc <diego.delacruz@mscdirect.com>
2024-07-09 15:52:12 -0600

    SETUP: DocsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Docs from "./pages/Docs";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/docs" element={<Docs />} />
+    </Routes>
   );
 }
 
