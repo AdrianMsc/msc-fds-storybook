a5aa13637f877f50e50da4d3bc78f5aa82392ee4
diegomsc <diego.delacruz@mscdirect.com>
2024-07-09 15:49:01 -0600

    FEAT: HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import Lottie from "lottie-react";
+import HomeLottie from "../assets/animation.json";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="max-w-[1000px] min-h-screen mx-auto flex items-center">
+      <section className="flex flex-col p-10 w-[50%] h-auto gap-6">
+        <h1 className="font-semibold text-5xl text-pretty">
+          Welcome to MSC Design System
+        </h1>
+        <p className="text-pretty text-xl ">
+          Empowering Innovation Through Unified Design.
+        </p>
+        <div className="h-fit flex gap-2 mt-2">
+          <Link
+            to="/Docs"
+            className="p-4 bg-blue-600 rounded-full text-white items-center flex gap-2"
+          >
+            Go to Docs
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
+          <a
+            href="https://mscdirectjira.atlassian.net/wiki/spaces/PD/pages/2251784264/Batch+Release+Notes"
+            target="blank"
+            className="p-4 bg-trasparent rounded-full text-black text-pretty items-center flex gap-2"
+          >
+            Batch Release notes
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </a>
+        </div>
+      </section>
+      <section className=" w-[50%]">
+        <Lottie animationData={HomeLottie} loop={true} />
+      </section>
+    </main>
+  );
 };
 
 export default Home;
