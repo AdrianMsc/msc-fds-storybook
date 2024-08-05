9521428cd1b3519d0e928924107b61c63d644a0a
diegomsc <diego.delacruz@mscdirect.com>
2024-08-05 12:41:28 -0600

    DOCS: Checkbox

--- src/pages/componentsDocs/CheckboxPage.jsx
+++ src/pages/componentsDocs/CheckboxPage.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Codeblock } from "../../components";
 
 const CheckboxPage = () => {
-  return <div>CheckboxPage</div>;
+  return (
+    <>
+      <small className="category-tag">Form</small>
+      <h1>Checkbox</h1>
+
+      <div className="msc-component-container-col">
+        <div className="msc-checkbox-container">
+          <input
+            type="checkbox"
+            id="checkbox-id"
+            value="checkbox-value"
+            className="msc-checkbox"
+          />
+          <label htmlFor="checkbox-id" className="msc-checkbox-label">
+            Checkbox label
+          </label>
+        </div>
+      </div>
+
+      <h1>Code</h1>
+      <Codeblock>
+        {`
+  <div class="msc-checkbox-container">
+    <input
+      type="checkbox"
+      id="checkbox-id"
+      value="checkbox-value"
+      class="msc-checkbox"
+    />
+      <label for="checkbox-id" class="msc-checkbox-label">
+        Checkbox label
+      </label>
+  </div>
+          `}
+      </Codeblock>
+    </>
+  );
 };
 
 export default CheckboxPage;
