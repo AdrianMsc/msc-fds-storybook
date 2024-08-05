30181a695b2d2640773f0c06c766a1d02bd62c94
diegomsc <diego.delacruz@mscdirect.com>
2024-08-05 12:39:34 -0600

    SETUP: Code highlight added

---
+++ src/components/Codeblock.jsx
@@ -0,0 +1,16 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+const Codeblock = ({ children }) => {
+  return (
+    <SyntaxHighlighter
+      language="htmlbars"
+      style={atomOneDark}
+      className="max-[1500px] mt-2 rounded"
+    >
+      {children}
+    </SyntaxHighlighter>
+  );
+};
+
+export default Codeblock;
