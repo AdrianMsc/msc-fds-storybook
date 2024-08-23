033eae562797156ec92535e1c27dbc7d410b09bc
AdrianLona <jadrianlf@gmail.com>
2024-08-23 14:40:19 -0600

    duh

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://adrianmsc.github.com/msc-fuel-design-system-react",
+  base: "https://adrianmsc.github.io/msc-fuel-design-system-react/",
 });
