e7eabf1bd61c69551887265897b7b80e8f72da79
AdrianMsc <AdrianMsc@users.noreply.github.com>
2024-08-26 15:08:07 +0000

    deploy: 25c98886d73b975354b95684a0d52de347aa5e3e

---
+++ assets/Button.stories-CyJPUySm.js
@@ -0,0 +1,20 @@
+import"./index-BPHu6psy.js";import{B as b}from"./Button-DcI1EO0y.js";import"./index-DZLKizrv.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";import"./index-C1dzxQ9A.js";const E={title:"Example/Button",component:b,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},r={args:{primary:!0,label:"Button"}},e={args:{label:"Button"}},a={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}};var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: "Button"
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,m,l;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: "Button"
+  }
+}`,...(l=(m=e.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var p,u,i;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    label: "Button"
+  }
+}`,...(i=(u=a.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var d,g,B;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    label: "Button"
+  }
+}`,...(B=(g=s.parameters)==null?void 0:g.docs)==null?void 0:B.source}}};const L=["Primary","Secondary","Large","Small"];export{a as Large,r as Primary,e as Secondary,s as Small,L as __namedExportsOrder,E as default};
