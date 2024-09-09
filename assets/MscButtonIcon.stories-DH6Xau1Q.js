dc2890c2e52cbc259827243637ea0d6eab0e4a4f
AdrianMsc <AdrianMsc@users.noreply.github.com>
2024-09-09 19:43:15 +0000

    deploy: 9c946eade560d445052a45ff7e6d8e29c26bbd39

--- assets/MscButtonIcon.stories-DH6Xau1Q.js
+++
@@ -1,6 +0,0 @@
-import{j as a}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";/* empty css               */const n=({icon:u="fa-solid fa-thumbs-up",size:c="default",disabled:r=!1})=>{const s={default:"",small:"-sm",large:"-lg"},i=`msc-icon-button${s[c]||s.default}`;return a.jsx("button",{className:i,disabled:r,children:a.jsx("i",{className:u})})};n.__docgenInfo={description:"",methods:[],displayName:"MscButtonIcon",props:{icon:{defaultValue:{value:'"fa-solid fa-thumbs-up"',computed:!1},required:!1},size:{defaultValue:{value:'"default"',computed:!1},required:!1},disabled:{defaultValue:{value:"false",computed:!1},required:!1}}};const p={title:"Action/ButtonIcon",component:n,parameters:{layout:"centered"},argTypes:{icon:{control:"select",options:["fa-solid fa-thumbs-up","fa-solid fa-magnifying-glass","fa-solid fa-comment"]},size:{control:"select",options:["default","small","large"]},disabled:{control:"boolean"}}},e={args:{icon:"fa-solid fa-thumbs-up",size:"default"}};var o,t,l;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    icon: "fa-solid fa-thumbs-up",
-    size: "default"
-  }
-}`,...(l=(t=e.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};const g=["mscButtonIcon"];export{g as __namedExportsOrder,p as default,e as mscButtonIcon};
