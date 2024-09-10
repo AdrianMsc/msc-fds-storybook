ca6ce15b0f2871400a3d213017cf7e439a5f46ec
AdrianMsc <AdrianMsc@users.noreply.github.com>
2024-09-10 21:20:36 +0000

    deploy: 415e0af091b4df7fecc79015e27c29eee05aad34

--- assets/MscShadow.stories-DS2K5IsJ.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const n=({size:a})=>{const o={small:"shadow-sm",default:"shadow",medium:"shadow-md",large:"shadow-lg",xLarge:"shadow-xl",xxLarge:"shadow-xl",inner:"shadow-inner"};return e.jsx(e.Fragment,{children:e.jsxs("div",{class:`size-[128px] rounded-md ${o[a]} text-fuchsia-600 text-center content-center font-bold`,children:[".",o[a]]})})},c=n;n.__docgenInfo={description:"",methods:[],displayName:"MscShadows"};const i={title:"Foundations/Shadows",component:c,parameters:{layout:"centered"},argTypes:{size:{control:"select",options:["small","default","medium","large","xLarge","xxLarge","inner"]}}},s={args:{size:"small"}};var r,t,d;s.parameters={...s.parameters,docs:{...(r=s.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    size: "small"
-  }
-}`,...(d=(t=s.parameters)==null?void 0:t.docs)==null?void 0:d.source}}};const h=["mscShadows"];export{h as __namedExportsOrder,i as default,s as mscShadows};
--- assets/MscSpacing.stories-BZW1CvHM.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const n=({size:r})=>s.jsxs(s.Fragment,{children:[s.jsxs("h2",{class:"mt-5",children:[r,"px"]}),s.jsx("div",{class:"p-1 outline outline-slate-300 my-4 min-w-[200px]",children:s.jsx("div",{class:`w-full h-[${r}px] bg-slate-300`})})]}),c=n;n.__docgenInfo={description:"",methods:[],displayName:"MscSpacing"};const l={title:"Foundations/Spacing",component:c,parameters:{layout:"centered"},argTypes:{size:{control:"select",options:[2,4,8,16,24]}}},e={args:{size:4}};var o,t,a;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    size: 4
-  }
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const m=["mscTypography"];export{m as __namedExportsOrder,l as default,e as mscTypography};
