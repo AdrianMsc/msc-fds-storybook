d68285ca6a0ba1ead1f183834ae40ccc1f248cce
diegodlc9 <diegodlc9@users.noreply.github.com>
2024-09-11 21:07:39 +0000

    deploy: ca451410e66bfa4620ddc789ef0895ba6ac11a11

---
+++ assets/MscCheckbox.stories-BZhjQYtR.js
@@ -0,0 +1,5 @@
+import{j as c}from"./jsx-runtime-CkxqCPlQ.js";/* empty css               */import"./index-DJO9vBfz.js";const a=({label:t})=>c.jsxs("div",{class:"msc-checkbox-container",children:[c.jsx("input",{type:"checkbox",id:"checkbox-id",value:"checkbox-value",class:"msc-checkbox"}),c.jsx("label",{for:"checkbox-id",class:"msc-checkbox-label",children:t})]}),l=a;a.__docgenInfo={description:"",methods:[],displayName:"MscCheckbox"};const m={title:"Form/Checkbox",component:l,parameters:{layout:"centered"},argTypes:{label:{control:"text"}}},e={args:{label:"Checkbox Label"}};var o,s,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    label: "Checkbox Label"
+  }
+}`,...(r=(s=e.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const h=["mscCheckbox"];export{h as __namedExportsOrder,m as default,e as mscCheckbox};
