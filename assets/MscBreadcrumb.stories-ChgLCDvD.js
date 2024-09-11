af202cb277c62a1ead4526b1cfbaf13a9117b783
diegodlc9 <diegodlc9@users.noreply.github.com>
2024-09-11 21:27:25 +0000

    deploy: 45614dfacb28a07725666b2dfcfbcd6b2edf6c03

--- assets/MscBreadcrumb.stories-ChgLCDvD.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const m=({steps:s=5})=>r.jsx("ol",{class:"msc-breadcrumb-container",children:Array.from({length:s}).map((d,a)=>r.jsxs("li",{children:[r.jsx("a",{className:"msc-breadcrumb",children:"breadcrumb"}),a<s-1&&r.jsx("span",{children:"/"})]},a))}),n=m;m.__docgenInfo={description:"",methods:[],displayName:"MscBreadcrumb",props:{steps:{defaultValue:{value:"5",computed:!1},required:!1}}};const l={title:"Navigation/Breadcrumb",component:n,parameters:{layout:"centered"},argTypes:{steps:{control:"number"}}},e={args:{steps:7}};var c,t,o;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
-  args: {
-    steps: 7
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const i=["mscBreadcrumb"];export{i as __namedExportsOrder,l as default,e as mscBreadcrumb};
--- assets/MscDivider.stories-CYpS_GKr.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-CkxqCPlQ.js";/* empty css               */import"./index-DJO9vBfz.js";const t=({variant:o="horizontal_gray"})=>{const n=`${{horizontal_gray:"msc-h-divider-gray",horizontal_blue:"msc-h-divider-blue",vertical_gray:"msc-v-divider-gray",vertical_blue:"msc-v-divider-blue"}[o]}`;return e.jsx("div",{class:"flex size-[400px] items-center justify-center py-3 px-4",children:e.jsx("div",{className:n})})},c=t;t.__docgenInfo={description:"",methods:[],displayName:"MscDivider",props:{variant:{defaultValue:{value:'"horizontal_gray"',computed:!1},required:!1}}};const p={title:"Form/Divider",component:c,parameters:{layout:"centered"},argTypes:{variant:{control:"select",options:["horizontal_gray","horizontal_blue","vertical_gray","vertical_blue"]}}},r={args:{variant:"horizontal_gray"}};var a,s,i;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    variant: "horizontal_gray"
-  }
-}`,...(i=(s=r.parameters)==null?void 0:s.docs)==null?void 0:i.source}}};const u=["mscDivider"];export{u as __namedExportsOrder,p as default,r as mscDivider};
