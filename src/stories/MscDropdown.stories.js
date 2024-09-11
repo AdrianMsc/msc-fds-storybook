ca451410e66bfa4620ddc789ef0895ba6ac11a11
diegomsc <diego.delacruz@mscdirect.com>
2024-09-11 15:06:19 -0600

    Feat: Dropdown done

---
+++ src/stories/MscDropdown.stories.js
@@ -0,0 +1,33 @@
+import MscDropdown from "../components/ds/MscDropdown";
+
+export default {
+  title: "Action/Dropdown",
+  component: MscDropdown,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    variant: {
+      control: "select",
+      options: [
+        "default",
+        "disabled",
+        "with label",
+        "with label on top",
+        "numeric",
+        "double",
+        "error",
+      ],
+    },
+    placeholder: { control: "text" },
+    reset: { control: "boolean" },
+  },
+};
+
+export const mscDropdown = {
+  args: {
+    variant: "default",
+    placeholder: "Select a Buyer",
+    reset: false,
+  },
+};
