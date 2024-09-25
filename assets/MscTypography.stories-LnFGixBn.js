b270666cb274eba3a0af55985c132b6332ffc2b1
diegodlc9 <diegodlc9@users.noreply.github.com>
2024-09-25 14:31:47 +0000

    deploy: 5c3acb9b3a84babdd11cdfea83fa5cc9904293a6

--- assets/MscTypography.stories-LnFGixBn.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const c=({text:s})=>r.jsxs(r.Fragment,{children:[r.jsx("h1",{children:s}),r.jsx("h2",{children:s}),r.jsx("h3",{children:s}),r.jsx("h4",{children:s}),r.jsx("h5",{children:s}),r.jsx("h6",{children:s})]}),p=c;c.__docgenInfo={description:"",methods:[],displayName:"MscTypography"};const h={title:"Foundations/Typography",component:p,parameters:{layout:"centered"},argTypes:{text:{control:"text"}}},e={args:{text:"Heading"}};var o,a,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    text: "Heading"
-  }
-}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const i=["mscTypography"];export{i as __namedExportsOrder,h as default,e as mscTypography};
