42ad2daa834b09c86ab2556806b685bcb955d0f1
diegomsc <diego.delacruz@mscdirect.com>
2024-10-02 10:19:19 -0600

    Remove: usless files

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Docs } from "./Docs";
-export { default as GettingStartedPage } from "./GettingStartedPage";
-export { default as ErrorPage } from "./ErrorPage";
