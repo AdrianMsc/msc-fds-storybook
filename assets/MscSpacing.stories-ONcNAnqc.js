ca538db4fca24d0995d8400647b96ac04d186208
AdrianLonaFragoso <AdrianLonaFragoso@users.noreply.github.com>
2025-01-07 16:29:42 +0000

    deploy: 06549a867245bd2fd4c5e2875b4f2363d7d7fb45

--- assets/MscSpacing.stories-ONcNAnqc.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const n=({size:a})=>s.jsxs(s.Fragment,{children:[s.jsxs("h2",{className:"mt-5",children:[a,"px"]}),s.jsx("div",{className:"p-1 outline outline-slate-300 my-4 min-w-[200px]",children:s.jsx("div",{className:`w-full h-[${a}px] bg-slate-300`})})]}),c=n;n.__docgenInfo={description:"",methods:[],displayName:"MscSpacing"};const m={title:"Foundations/Spacing",component:c,parameters:{layout:"centered"},argTypes:{size:{control:"select",options:[2,4,8,16,24]}}},e={args:{size:4}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    size: 4
-  }
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const l=["mscTypography"];export{l as __namedExportsOrder,m as default,e as mscTypography};
